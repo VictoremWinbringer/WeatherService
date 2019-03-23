13f7dc54592cb53a01b218d6a35befe1e67ecf04
VictoremWinbringer <victor@mail.ru>
2019-03-23 12:49:00 +0300

    added actix_web create

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate actix_web;
+use actix_web::{http, server, App, Path, Responder};
+
+fn weather(info: Path<(String, String)>) -> impl Responder {
+    format!("Weather in City: {}  for Period {}", info.0, info.1)
+}
+
 fn main() {
-    println!("Hello, world!");
+    server::new(
+        || App::new()
+            .route("/api/v1/weather/{city}/{period}", http::Method::GET, weather))
+        .bind("127.0.0.1:8080")
+        .unwrap()
+        .run()
+    ()
 }
