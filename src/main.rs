2a4714386cc5667208cf0238981476abfa418179
Victor Winbringer <victorwinbringer@MacBook-Pro-Victor.local>
2019-03-23 14:12:49 +0300

    experement with response

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 extern crate actix_web;
-use actix_web::{http, server, App, Path, Responder};
+use actix_web::{http, server, App, Path, Responder, HttpRequest, HttpResponse};
 
 fn weather(info: Path<(String, String)>) -> impl Responder {
-    format!("Weather in City: {}  for Period {}", info.0, info.1)
+    if info.0 == "Moscow" {
+        HttpResponse::Ok()
+            .content_type("text/html")
+            .body(format!("Weather in City: {}  for Period {}", info.0, info.1))
+
+    } else {
+     HttpResponse::NotFound()
+         .content_type("text/html")
+         .body(format!("Weather in City: {} Not found!", info.0))
+    }
 }
 
 fn main() {
     server::new(
         || App::new()
-            .route("/api/v1/weather/{city}/{period}", http::Method::GET, weather))
+            .prefix("/api/v1")
+            .route("/weather/{city}/{period}", http::Method::GET, weather))
         .bind("127.0.0.1:8080")
         .unwrap()
         .run()
-    ()
 }
