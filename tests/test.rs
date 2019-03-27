295cfe5e1a4a771aad00e047411dfafa45ae1c29
Victor Winbringer <victorwinbringer@MacBook-Pro-Victor.local>
2019-03-27 23:49:25 +0300

    refactoring tests

--- tests/test.rs
+++ tests/test.rs
@@ -3,35 +3,14 @@ use weather_service;
 use std::thread;
 use actix_web::http::StatusCode;
 
-#[test]
-fn get_weather_today() -> Result<(), Box<std::error::Error>> {
-    let _ = thread::spawn(|| {
-        weather_service::run("127.0.0.1:8080");
-    });
-    let mut response = reqwest::get("http://127.0.0.1:8080/api/v1/weather/RU/test_city/1day")?;
-    assert!(response.status() == 200, "{}", response.text()?);
-    assert_eq!(r#"{"temperature":3.0}"#, response.text()?);
-    Ok(())
-}
 
 #[test]
-fn get_weather_for_week() -> Result<(), Box<std::error::Error>> {
-    let _ = thread::spawn(|| {
-        weather_service::run("127.0.0.1:8081");
-    });
-    let mut response = reqwest::get("http://127.0.0.1:8081/api/v1/weather/RU/test_city/5day")?;
-    assert!(response.status() == 200, "{}", response.text()?);
-    assert_eq!(r#"[{"temperature":1.5},{"temperature":3.0},{"temperature":4.5},{"temperature":6.0},{"temperature":7.5}]"#, response.text()?);
-    Ok(())
-}
-
-#[test]
-fn get_weather_service_should_return_not_found_for_uncorrected_period() -> Result<(), Box<std::error::Error>> {
+fn get_weather_service_should_return_error_for_uncorrected_period() -> Result<(), Box<std::error::Error>> {
     let _ = thread::spawn(|| {
         weather_service::run("127.0.0.1:8082");
     });
     let mut response = reqwest::get("http://127.0.0.1:8082/api/v1/weather/RU/test_city/sasdfa")?;
-    assert!(response.status() == 404, "{}", response.text()?);
+    assert!(response.status() == 500, "{}", response.text()?);
     Ok(())
 }
 
